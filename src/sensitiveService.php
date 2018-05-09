88d09dddcc85cfdc3cdf6206d07f88ca5ef82b16
duanchilde <673857675@qq.com>
2018-05-09 11:34:01 +0800

    first

---
+++ src/sensitiveService.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: duanwei
+ * Date: 2018/5/9
+ * Time: 上午11:28
+ */
+
+class sensitiveService{
+
+    public function validate($content,$level=null)
+    {
+        return true;
+    }
+
+
+}
